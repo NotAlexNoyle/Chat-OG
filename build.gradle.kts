8bd3fa9cfc99e1517c8eaade6c286fb07e7b537e
Sven <skbotnl@disroot.org>
2026-08-01 00:25:27 +0200

    Update Kotlin to 2.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("com.diffplug.spotless") version "8.1.0" // Import Spotless plugin.
     id("com.gradleup.shadow") version "8.3.9" // Import Shadow plugin.
     eclipse // Import Eclipse plugin.
-    kotlin("jvm") version "2.1.21" // Import Kotlin JVM plugin.
+    kotlin("jvm") version "2.4.10" // Import Kotlin JVM plugin.
 }
 
 /* ---------------------------- Java / Kotlin -------------------------- */
